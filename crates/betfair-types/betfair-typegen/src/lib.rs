d9c68ad3cc3a3538ad4a33160abd9f79c05c8d9e
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2023-05-01 15:59:35 +0300

    chore: add hardcore linter rules

--- crates/betfair-types/betfair-typegen/src/lib.rs
+++ crates/betfair-types/betfair-typegen/src/lib.rs
@@ -1,3 +1,9 @@
+#![warn(missing_docs, unreachable_pub, unused_crate_dependencies)]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![doc(test(
+    no_crate_inject,
+    attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
+))]
 pub mod gen_v1;
 
 pub use rust_decimal;
