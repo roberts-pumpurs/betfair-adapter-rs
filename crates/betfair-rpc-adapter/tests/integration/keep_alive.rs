c9ace06df8c85ee77a558be5258051b8d96db37e
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2023-12-16 22:16:13 +0200

    test: keep alive endpoint test

---
+++ crates/betfair-rpc-adapter/tests/integration/keep_alive.rs
@@ -0,0 +1,37 @@
+use rstest::rstest;
+use serde_json::json;
+use wiremock::matchers::{header, method, path};
+use wiremock::{Mock, ResponseTemplate};
+
+use crate::utils::{Server, APP_KEY, KEEP_ALIVE_URL, SESSION_TOKEN};
+
+#[rstest]
+#[test_log::test(tokio::test)]
+async fn keep_alive() {
+    let server = Server::new().await;
+
+    // Setup
+    let response = json!(
+        {
+            "token":"SESSIONTOKEN",
+            "product":"AppKey",
+            "status":"SUCCESS",
+            "error":""
+        }
+    );
+    Mock::given(method("GET"))
+        .and(path(KEEP_ALIVE_URL))
+        .and(header("Accept", "application/json"))
+        .and(header("X-Authentication", SESSION_TOKEN))
+        .and(header("X-Application", APP_KEY))
+        .respond_with(ResponseTemplate::new(200).set_body_json(response))
+        .expect(1)
+        .named("Keep alive")
+        .mount(&server.bf_api_mock_server)
+        .await;
+
+    // Action
+    let client = server.client().await;
+    let client = client.authenticate().await.unwrap();
+    let _result = client.keep_alive().await.unwrap();
+}
