0cedebee0e138e60a0cf4bcb979cf33503070268
Roberts Pumpurs <roberts@pumpurlabs.com>
2025-12-06 16:43:47 +0200

    add an extra test

--- crates/betfair-types/tests/list_current_orders.rs
+++ crates/betfair-types/tests/list_current_orders.rs
@@ -16,3 +16,24 @@ fn parse_fixture() {
     serde_json::from_str::<Response<list_current_orders::ReturnType, serde_json::Value>>(&data)
         .unwrap();
 }
+
+#[test]
+fn parse_fixture_missing_matched_date() {
+    use betfair_types::types::sports_aping::list_current_orders;
+    use json_rpc_types::Response;
+
+    let data =
+        std::fs::read_to_string("./tests/resources/list_current_orders_missing_matched_date.json")
+            .unwrap();
+
+    let response: Response<list_current_orders::ReturnType, serde_json::Value> =
+        serde_json::from_str(&data).unwrap();
+
+    let orders = response
+        .payload
+        .expect("response should be successful")
+        .current_orders;
+
+    assert_eq!(orders.len(), 1);
+    assert!(orders[0].matched_date.is_none());
+}
